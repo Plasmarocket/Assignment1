bdb5c20f4dc279ae7090964a559a54d760d6b981
Plasmarocket <59854769+Plasmarocket@users.noreply.github.com>
2020-06-29 19:00:37 -0400

    create footer and partials folder

--- JsAssignment1/routes/index.js
+++ JsAssignment1/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { home: 'Express' });
+    res.render('Home', { home: 'Express' });
 });
 
 module.exports = router;
